ff93f6f64026be3e0a23e487e07c951c705ba902
Antoine Beauvais-Lacasse <abeaulac@octhuber.com>
2017-05-18 15:37:23 -0400

    Initial commit

---
+++ types/digits-web-sdk/index.d.ts
@@ -0,0 +1,63 @@
+// Type definitions for digits-web-sdk v1
+// Project: https://docs.fabric.io/web/digits/installation.html | https://cdn.digits.com/1/sdk.js
+// Definitions by: OctHuber Inc. / Antoine Beauvais-Lacasse <abeaulac@octhuber.com>
+
+import DigitsInitOptions = Digits.DigitsInitOptions;
+import ZeptoPromise = Digits.ZeptoPromise;
+import DigitsResponse = Digits.DigitsResponse;
+import DigitsEmbedOptions = Digits.DigitsEmbedOptions;
+
+interface Digits {
+    init(options: DigitsInitOptions): void;
+    isInitialized(): boolean;
+    getLoginStatus(): ZeptoPromise<DigitsResponse>;
+    logIn(): ZeptoPromise<DigitsResponse>;
+    embed(options: DigitsEmbedOptions): ZeptoPromise<DigitsResponse>;
+}
+
+declare namespace Digits {
+    /**
+     * The Digits web SDK uses the Zepto library, specifically zepto-deferred module:
+     * https://github.com/madrobby/zepto/blob/master/src/deferred.js
+     * Which is apparently: https://github.com/sudhirj/simply-deferred under the hood.
+     */
+    type ZeptoPromiseState = 'pending' | 'resolved' | 'rejected';
+    type ZeptoWhenable<TResult> = ZeptoPromise<TResult> | TResult;
+    type ZeptoSuccessCB<T, TResult> = (value: T) => ZeptoWhenable<TResult>;
+    type ZeptoFailureCB<TResult> = (err: any) => ZeptoWhenable<TResult>;
+
+    interface ZeptoPromise<T> extends PromiseLike<T> {
+        always(value: ZeptoWhenable<T>): ZeptoPromise<T>;
+
+        then<TResult>(onFulfill?: ZeptoSuccessCB<T, TResult>,
+                      onReject?: ZeptoFailureCB<TResult>): ZeptoPromise<TResult>;
+
+        done<TResult>(successCb: ZeptoSuccessCB<T, TResult>): ZeptoPromise<TResult>;
+
+        fail<TResult>(onReject: ZeptoFailureCB<TResult>): ZeptoPromise<TResult>;
+
+        state(): ZeptoPromiseState;
+
+        promise(target?: any): ZeptoPromise<T>;
+
+        // Some additional methods we don't care too much about...
+    }
+
+    interface DigitsInitOptions {
+        consumerKey: string;
+    }
+
+    interface DigitsEmbedOptions {
+        container: string;
+    }
+
+    interface DigitsOAuthEchoHeaders {
+        oauth_echo_header: string;
+        oauth_echo_service: string;
+    }
+
+    interface DigitsResponse {
+        status: string;
+        oauth_echo_headers: DigitsOAuthEchoHeaders;
+    }
+}
