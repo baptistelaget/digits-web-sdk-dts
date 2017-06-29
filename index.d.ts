67b15957255349b8b3c992b09130c17581893005
Antoine Beauvais-Lacasse <abeaulac@octhuber.com>
2017-06-29 12:47:40 -0400

    Bundle external zepto definitions, declare global Digits

--- index.d.ts
+++ index.d.ts
@@ -1,63 +1,40 @@
-// Type definitions for digits-web-sdk v1
-// Project: https://docs.fabric.io/web/digits/installation.html | https://cdn.digits.com/1/sdk.js
-// Definitions by: OctHuber Inc. / Antoine Beauvais-Lacasse <abeaulac@octhuber.com>
+// Type definitions for digits-web-sdk 1.x
+// Project: https://docs.fabric.io/web/digits/installation.html; https://cdn.digits.com/1/sdk.js
+// Definitions by: Antoine Beauvais-Lacasse <https://github.com/beaulac>
 
-/**
- * The Digits web SDK uses the Zepto library, specifically zepto-deferred module:
- * https://github.com/madrobby/zepto/blob/master/src/deferred.js
- * Which is apparently: https://github.com/sudhirj/simply-deferred under the hood.
- */
-type ZeptoPromiseState = 'pending' | 'resolved' | 'rejected';
-type ZeptoWhenable<TResult> = ZeptoPromise<TResult> | TResult;
-type ZeptoSuccessCB<T, TResult> = (value: T) => ZeptoWhenable<TResult>;
-type ZeptoFailureCB<TResult> = (err: any) => ZeptoWhenable<TResult>;
-
-interface ZeptoPromise<T> extends PromiseLike<T> {
-    always(value: ZeptoWhenable<T>): ZeptoPromise<T>;
-
-    then<TResult>(onFulfill?: ZeptoSuccessCB<T, TResult>,
-                  onReject?: ZeptoFailureCB<TResult>): ZeptoPromise<TResult>;
-
-    done<TResult>(successCb: ZeptoSuccessCB<T, TResult>): ZeptoPromise<TResult>;
-
-    fail<TResult>(onReject: ZeptoFailureCB<TResult>): ZeptoPromise<TResult>;
-
-    state(): ZeptoPromiseState;
-
-    promise(target?: any): ZeptoPromise<T>;
-
-    // Some additional methods we don't care too much about...
-}
-
-export interface DigitsInitOptions {
+interface DigitsInitOptions {
     consumerKey: string;
 }
 
-export interface DigitsEmbedOptions {
+interface DigitsEmbedOptions {
     container: string;
 }
 
-export interface DigitsStatusResponse {
+interface DigitsStatusResponse {
     readonly status: string;
     readonly oauth_echo_headers?: DigitsStatusOAuthEchoHeaders;
 }
-export interface DigitsStatusOAuthEchoHeaders {
+
+interface DigitsStatusOAuthEchoHeaders {
     readonly oauth_echo_header?: string;
     readonly oauth_echo_service?: string;
 }
 
-export interface DigitsLoginResponse {
+interface DigitsLoginResponse {
     readonly oauth_echo_headers: DigitsLoginOAuthEchoHeaders;
 }
-export interface DigitsLoginOAuthEchoHeaders {
+
+interface DigitsLoginOAuthEchoHeaders {
     readonly 'X-Auth-Service-Provider': string;
     readonly 'X-Verify-Credentials-Authorization': string;
 }
 
-export interface DigitsStatic {
+interface DigitsStatic {
     init(options: DigitsInitOptions): void;
     isInitialized(): boolean;
     getLoginStatus(): ZeptoPromise<DigitsStatusResponse>;
     logIn(): ZeptoPromise<DigitsLoginResponse>;
     embed(options: DigitsEmbedOptions): ZeptoPromise<DigitsLoginResponse>;
 }
+
+declare let Digits: DigitsStatic;
